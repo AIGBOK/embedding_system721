036f7aa9c68c9039153a1e1c0adfa3029173eca6
AIGBOK <71751426+AIGBOK@users.noreply.github.com>
2025-05-12 15:44:50 +0800

    Update pathLossModel_version2.py

--- pathLossModel_version2.py
+++ pathLossModel_version2.py
@@ -4,6 +4,7 @@ import math
 # 輸入你想要抓的點（依序對應每組資料）
 target_points = ['4', '2_2', '2_1', '1', '3_1', '3_2']
 csv_file = "C:\\Users\\chouy\\Desktop\\embedding_system\\Documents0506\\0506_rssi_output.csv"
+output_path = "C:\\Users\\chouy\\Desktop\\embedding_system\\Documents0506\\pathLoss_output.csv"
 
 #0430的測資
 # target_points = ['4']
@@ -43,12 +44,15 @@ for i, point_num in enumerate(point_numbers):
 
     extracted_dbms.append(dbm_group)
 
-
+#用result_db0來儲存做三角定位時，需要用到的數值
+result_db0 = []
 # 顯示結果
 for idx, group in enumerate(extracted_dbms):
     print(f"Group {idx + 1}: {group}")
+    result_db0.append(group[0])
 
 # 開始做path loss model
+averagePathLoss = []
 for idx, group in enumerate(extracted_dbms):
     v1 = group[0]
     results = []
@@ -60,7 +64,13 @@ for idx, group in enumerate(extracted_dbms):
         results.append(result)
         distance += 1
     print(f"Group {idx + 1}: log10 ratios relative to first value = {results}")
+    #針對每一組計算平均的path loss
+    average = sum(results) / len(results)
+    averagePathLoss.append(average)
 
+print("average Path Loss:")
+for idx, group in enumerate(averagePathLoss):
+    print(f"Group {idx + 1} = {group}")
 
 # 開始做path loss model
 # for idx, group in enumerate(extracted_dbms):
@@ -74,3 +84,13 @@ for idx, group in enumerate(extracted_dbms):
 #         results.append(result)
 #         k += 1
 #     print(f"Group {idx + 1}: log10 ratios relative to first value = {results}")
+
+#我要輸出db0跟path loss 係數，來做三角定位的計算
+#we have result_db0 來儲存distance = 1，的dbm
+#we have averagePathLoss 來儲存每一組的path loss
+with open(output_path, "w", newline='', encoding="utf-8") as f:
+    writer = csv.writer(f)
+    writer.writerow(["db0"] + result_db0)  # 合併成一列寫出
+    writer.writerow(["PathLossCoeff"] + averagePathLoss)
+
+print(f"✅ 已儲存至：{output_path}")
